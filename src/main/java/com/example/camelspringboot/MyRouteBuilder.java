3c7134bb6d79b0e5b2dd3549df52216f5f30a4d1
Giovanni Condello <giovanni.condello@coderit.it>
2023-05-30 15:10:10 +0200

    Splitter & aggregator

--- src/main/java/com/example/camelspringboot/MyRouteBuilder.java
+++ src/main/java/com/example/camelspringboot/MyRouteBuilder.java
@@ -3,58 +3,74 @@ package com.example.camelspringboot;
 
 import org.apache.camel.AggregationStrategy;
 import org.apache.camel.Exchange;
-import org.apache.camel.Predicate;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.builder.AggregationStrategies;
 import org.apache.camel.builder.FlexibleAggregationStrategy;
 import org.apache.camel.builder.RouteBuilder;
-import org.slf4j.ILoggerFactory;
+import org.apache.camel.model.dataformat.CsvDataFormat;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Component
 public class MyRouteBuilder extends RouteBuilder {
 
-    private static class AggregateToListStrategy implements AggregationStrategy {
 
-        @Override
-        public Exchange aggregate(Exchange currentOutput, Exchange currentInput) {
-            if (currentOutput == null) {
-                String body = currentInput.getMessage().getBody(String.class);
-                ArrayList<Object> aggregatedBody = new ArrayList<>();
-                aggregatedBody.add(body);
-                currentInput.getMessage().setBody(aggregatedBody);
-                return currentInput;
-            }
-            List aggregatedBody = currentOutput.getMessage().getBody(List.class);
-            String body = currentInput.getMessage().getBody(String.class);
-            aggregatedBody.add(body);
-            currentOutput.getMessage().setBody(aggregatedBody);
-            return currentOutput;
-        }
-    }
-
+    private final AtomicLong al = new AtomicLong(0);
 
     @Override
     public void configure() throws Exception {
-        from("timer:my-schedule?delay=350")
-                .routeId("generate-messages")
-                .process(exchange -> {
-                    exchange.getMessage().setBody(UUID.randomUUID().toString());
-                    exchange.getMessage().setHeader("X-Batch-Id", ((int) (Math.random() * 5)));
-                })
-                .log("Messaggio generato: ${body} ${header.X-Batch-Id}")
-                .filter(exchange -> exchange.getMessage().getBody(String.class).startsWith("9"))
-                .log("Messaggio sopravvissuto: ${body} ${header.X-Batch-Id}")
-                .aggregate(header("X-Batch-Id"), new FlexibleAggregationStrategy<String>()
-                        //.condition(exchange -> exchange.getMessage().getBody(String.class).startsWith("9"))
-                        .pick(body())
-                        .castAs(String.class)
-                        .accumulateInCollection(ArrayList.class)
-                        .storeInBody()
-                )
-                .completionInterval(30000)
-                .log("Dopo l'aggregazione ho ${body} ${header.X-Batch-Id}");
+
+        var csvDf = new CsvDataFormat();
+        csvDf.setLazyLoad("true");
+        csvDf.setUseMaps("true");
+        csvDf.setUseOrderedMaps("true");
+        csvDf.setSkipHeaderRecord("true");
+
+        from("file:input?includeExt=csv")
+                .log("Ricevuto file ${header.CamelFileName}")
+                .multicast(AggregationStrategies.useOriginal(), true)
+                    .to(ExchangePattern.InOut, "direct:elaborazione-b")
+                    .to(ExchangePattern.InOut,"direct:elaborazione-a")
+                .end()
+                .log("Ho terminato entrambe le elaborazioni")
+
+        ;
+
+        from("direct:elaborazione-b")
+                .unmarshal(csvDf)
+                .split(body()).streaming()
+                .aggregate(simple("${body[DESTINATION_AIRPORT]}"), AggregationStrategies.groupedBody())
+                .completionTimeout(3_000)
+                .log("Gruppo ${exchangeProperty.CamelAggregatedCorrelationKey} con ${body.size} elementi");
+
+        from("direct:elaborazione-a")
+                .unmarshal(csvDf)
+                .split(body(), (oldExchange, newExchange) -> {
+                    if (oldExchange == null) {
+                        String airport = newExchange.getMessage().getBody(String.class);
+                        HashMap<String, Long> hm = new HashMap<>();
+                        hm.put(airport, 1L);
+                        newExchange.getMessage().setBody(hm);
+                        return newExchange;
+                    }
+                    Map<String, Long> hm = oldExchange.getMessage().getBody(Map.class);
+                    String airport = newExchange.getMessage().getBody(String.class);
+                    hm.compute(airport, (k, v) -> v == null ? 1L : v + 1L);
+                    return oldExchange;
+                }).streaming()
+                .to("direct:line")
+                .end()
+                .log("Ci sono in tutto ${body.size()} aereoporti: ${body}");
+
+
+        from("direct:line")
+                .setBody().body(b -> ((Map) b).get("DESTINATION_AIRPORT"))
+        //.log("-> ${body}");
+        ;
     }
 }
