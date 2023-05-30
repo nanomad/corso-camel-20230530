f7709d1e02e8dbb8e3f3ebf9eacc41d1027fcc4e
Giovanni Condello <giovanni.condello@coderit.it>
2023-05-30 10:57:18 +0200

    Add aggregation example

--- src/main/java/com/example/camelspringboot/MyRouteBuilder.java
+++ src/main/java/com/example/camelspringboot/MyRouteBuilder.java
@@ -1,63 +1,60 @@
 package com.example.camelspringboot;
 
 
+import org.apache.camel.AggregationStrategy;
 import org.apache.camel.Exchange;
-import org.apache.camel.LoggingLevel;
+import org.apache.camel.Predicate;
+import org.apache.camel.builder.FlexibleAggregationStrategy;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.ILoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Component
 public class MyRouteBuilder extends RouteBuilder {
 
-    private static final String MY_FILENAME_PROP = "X-Original-Filename";
+    private static class AggregateToListStrategy implements AggregationStrategy {
 
-    static class MyBean {
-        public String setToUpperCase(String message) {
-            return message.toUpperCase(Locale.ENGLISH);
+        @Override
+        public Exchange aggregate(Exchange currentOutput, Exchange currentInput) {
+            if (currentOutput == null) {
+                String body = currentInput.getMessage().getBody(String.class);
+                ArrayList<Object> aggregatedBody = new ArrayList<>();
+                aggregatedBody.add(body);
+                currentInput.getMessage().setBody(aggregatedBody);
+                return currentInput;
+            }
+            List aggregatedBody = currentOutput.getMessage().getBody(List.class);
+            String body = currentInput.getMessage().getBody(String.class);
+            aggregatedBody.add(body);
+            currentOutput.getMessage().setBody(aggregatedBody);
+            return currentOutput;
         }
     }
 
+
     @Override
     public void configure() throws Exception {
-        onException(Exception.class).maximumRedeliveries(1);
-
-        getCamelContext().setTracing(true);
-
-
-        from("file:input") // Exchange creata qui
-                .routeId("read-input-folder")
-                .to("seda:process-file?size=10&blockWhenFull=true");
-
-        from("seda:process-file?concurrentConsumers=10")
-                .routeId("process-input-file-parallel")
-                .log(LoggingLevel.INFO, "Ricevuto un nuovo file: ${header.CamelFileNameOnly}")
+        from("timer:my-schedule?delay=350")
+                .routeId("generate-messages")
                 .process(exchange -> {
-                    var body = exchange.getMessage().getBody(String.class);
-                    String[] tokens = body.split(";", -1);
-                    String stringa = String.format("latitude=%s&longitude=%s", tokens[0], tokens[1]);
-                    exchange.getMessage().setBody(stringa);
+                    exchange.getMessage().setBody(UUID.randomUUID().toString());
+                    exchange.getMessage().setHeader("X-Batch-Id", ((int) (Math.random() * 5)));
                 })
-                .setProperty(MY_FILENAME_PROP, header(Exchange.FILE_NAME_ONLY))
-                .to("direct:call-forecasts-api")
-                .to("direct:write-output-file");
-
-
-        from("direct:call-forecasts-api")
-                .routeId("call-forecasts-api")
-                .setHeader(Exchange.HTTP_QUERY, simple("${body}"))
-                .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-                .to("https://api.open-meteo.com/v1/forecast").id("get-forecasts")
-                .unmarshal().json(JsonLibrary.Jackson);
-
-        from("direct:write-output-file")
-                .routeId("write-output-file")
-                .marshal().csv()
-                .log("${body}")
-                .log("File ${header.CamelFileNameOnly} spostato correttamente")
-                .toD("file:output?fileExist=Override&fileName=${exchangeProperty.X-Original-Filename}").id("to-write-output-file");
-        // Exchange distrutta qui
+                .log("Messaggio generato: ${body} ${header.X-Batch-Id}")
+                .filter(exchange -> exchange.getMessage().getBody(String.class).startsWith("9"))
+                .log("Messaggio sopravvissuto: ${body} ${header.X-Batch-Id}")
+                .aggregate(header("X-Batch-Id"), new FlexibleAggregationStrategy<String>()
+                        //.condition(exchange -> exchange.getMessage().getBody(String.class).startsWith("9"))
+                        .pick(body())
+                        .castAs(String.class)
+                        .accumulateInCollection(ArrayList.class)
+                        .storeInBody()
+                )
+                .completionInterval(30000)
+                .log("Dopo l'aggregazione ho ${body} ${header.X-Batch-Id}");
     }
 }
